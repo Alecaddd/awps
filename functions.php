84ee4b51dd521a43c4f1260ee97e37572a7adec9
Alecaddd <castellani.ale@gmail.com>
2016-08-25 22:05:45 -0700

    added autoload to init the base class

--- functions.php
+++ functions.php
@@ -5,37 +5,15 @@
  *
  * This theme uses classes and OOP logic instead of procedural coding
  * Every function, hook and action is properly divided and organized inside related files
- * Use the file `inc/custom.php` to write your custom functions
+ * Use the file `config/custom.php` to write your custom functions
  *
  */
 
-// Base Controller
-require get_template_directory() . '/config/baseController.php';
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ):
+  require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
-// Classes
-require get_template_directory() . '/config/setup.php';
-require get_template_directory() . '/config/menus.php';
-require get_template_directory() . '/config/widgets.php';
-require get_template_directory() . '/config/enqueue.php';
-require get_template_directory() . '/config/custom-header.php';
-require get_template_directory() . '/config/template-tags.php';
-require get_template_directory() . '/config/extras.php';
-require get_template_directory() . '/config/customizer.php';
-require get_template_directory() . '/config/jetpack.php';
+endif;
 
-
-// Define and init all the classes
-$awps = new baseController;
-
-$awps -> setup = new setupController;
-$awps -> menus = new menusController;
-$awps -> widgets = new widgetsController;
-$awps -> enqueue = new enqueueController;
-$awps -> header = new headerController;
-$awps -> tags = new tagsController;
-$awps -> extras = new extrasController;
-$awps -> customizer = new customizerController;
-$awps -> jetpack = new jetpackController;
-
-// Call your custom function
-$awps -> custom = new customController;
+if( class_exists( 'awps\\setup' ) ):
+   new \awps\setup();
+endif;
