172468c186877defa49536a3e3cb5f4ce0cc4dea
Alecaddd <castellani.ale@gmail.com>
2016-08-29 21:19:48 -0700

    get_template_parts() pointing to the right location

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'views/content', get_post_format() );
+				get_template_part( 'src/views/content/content', get_post_format() );
 
 			endwhile;
 
@@ -30,7 +30,7 @@ get_header(); ?>
 
 		else :
 
-			get_template_part( 'views/content', 'none' );
+			get_template_part( 'src/views/content/content', 'none' );
 
 		endif; ?>
 
