3e275c5ec7a78c1a67bd8573de6369e3314ded47
Alecaddd <alecaddd@Alecaddds-iMac.local>
2016-12-27 20:13:02 -0800

    Updated APP_ENV detection for browsersync

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php if (defined('ENV') && ENV === 'dev'): ?>
+<?php if (getenv('APP_ENV') === 'development'): ?>
 
 	 <script id="__bs_script__">//<![CDATA[
 		document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.17.5'><\/script>".replace("HOST", location.hostname));
