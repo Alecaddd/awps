86503d7038d852d8a3a39604a3f4c1cc17e37c8c
Alecaddd <castellani.ale@gmail.com>
2017-05-06 15:55:43 -0700

    Add archive page

---
+++ archive.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package awps
+ */
+
+get_header(); ?>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-sm-8">
+
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+
+				<?php
+				if ( have_posts() ) : ?>
+
+					<header>
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+					</header>
+
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'template-parts/content', get_post_format() );
+
+					endwhile;
+
+					the_posts_navigation();
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif; ?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+
+		</div><!-- .col- -->
+
+		<div class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</div><!-- .col- -->
+
+	</div><!-- .row -->
+
+</div><!-- .container -->
+
+<?php
+get_footer();
