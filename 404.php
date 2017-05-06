5984887b08cb1c0532be531ad1841691febf78b2
Alecaddd <castellani.ale@gmail.com>
2017-05-05 23:38:28 -0700

    PHPCS fixes

--- 404.php
+++ 404.php
@@ -1,7 +1,11 @@
 <?php
 /**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * 
  * @package awps
-*/
+ */
 
 get_header(); ?>
 
@@ -9,21 +13,21 @@ get_header(); ?>
 
 	<div class="row">
 
-    	<div class="col-sm-8">
+		<div class="col-sm-8">
 
-	    	<div id="primary" class="content-area">
-	        	<main id="main" class="site-main" role="main">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
 
-	    			<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'awps' ); ?></h1>
+					<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'awps' ); ?></h1>
 
-	        	</main><!-- #main -->
-	      	</div><!-- #primary -->
+				</main><!-- #main -->
+			</div><!-- #primary -->
 
-    	</div><!-- .col- -->
+		</div><!-- .col- -->
 
-	    <div class="col-sm-4">
-	    	<?php get_sidebar(); ?>
-	    </div><!-- .col- -->
+		<div class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</div><!-- .col- -->
 
 	</div><!-- .row -->
 
