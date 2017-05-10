37eec0338085964ddb33735289db4da93ff4d233
Alecaddd <castellani.ale@gmail.com>
2017-05-09 20:22:15 -0700

    Compiled assets

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -9,36 +9,36 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-(function($) {
+(function( $ ) {
 
 	// Site title and description.
-	wp.customize('blogname', awps_updateSiteText('.site--title a', value));
+	wp.customize( 'blogname', awpUpdateSiteText( '.site--title a', value ) );
 
-	wp.customize('blogdescription', awps_updateSiteText('.site-description', value));
+	wp.customize( 'blogdescription', awpUpdateSiteText( '.site-description', value ) );
 
-	function awps_updateSiteText(selector, value) {
-		value.bind(function(to) {
-			$(selector).text(to);
+	function awpUpdateSiteText( selector, value ) {
+		value.bind( function( to ) {
+			$( selector ).text( to );
 		});
 	}
 
 	// Header text color.
-	wp.customize('header_textcolor', function(value) {
-		value.bind(function(to) {
-			if ('blank' === to) {
-				$('.site-title a, .site-description').css({
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title a, .site-description' ).css({
 					'clip': 'rect(1px, 1px, 1px, 1px)',
 					'position': 'absolute'
 				});
 			} else {
-				$('.site-title a, .site-description').css({
+				$( '.site-title a, .site-description' ).css({
 					'clip': 'auto',
 					'position': 'relative'
 				});
-				$('.site-title a, .site-description').css({
+				$( '.site-title a, .site-description' ).css({
 					'color': to
 				});
 			}
 		});
 	});
-})(jQuery);
+}( jQuery ) );
