4339307a18b8f3fec6db08d16a827bca776ce89d
Alecaddd <castellani.ale@gmail.com>
2017-05-18 22:50:09 -0700

    Implemented default site info in footer

--- footer.php
+++ footer.php
@@ -13,8 +13,18 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer container-fluid" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php
+				/* translators: %s: Github repo URL. */
+				echo esc_url( __( 'https://github.com/Alecaddd/awps', 'awps' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'awps' ), 'AWPS' ); ?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: %1: Theme name. */
 
+				/* translators: %2: Author name. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'awps' ), 'AWPS', '<a href="http://alecaddd.com/" rel="designer">Alecaddd</a>' ); ?>
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
