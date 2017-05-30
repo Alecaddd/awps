a0d32e647ea84af7bf0dbc08982e7ecf65b3aac3
Alecaddd <castellani.ale@gmail.com>
2017-05-29 18:45:08 -0700

    Hide edited post date

--- config/core/tags.php
+++ config/core/tags.php
@@ -20,13 +20,13 @@ class tags
     {
         $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
         if (get_the_time('U') !== get_the_modified_time('U')) {
-            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated hide" datetime="%3$s">%4$s</time>';
         }
-        $time_string = sprintf($time_string,
-            esc_attr(get_the_date('c')),
-            esc_html(get_the_date()),
-            esc_attr(get_the_modified_date('c')),
-            esc_html(get_the_modified_date())
+        $time_string = sprintf( $time_string,
+            esc_attr( get_the_date( 'c' ) ),
+            esc_html( get_the_date() ),
+            esc_attr( get_the_modified_date( 'c' ) ),
+            esc_html( get_the_modified_date() )
         );
         $posted_on = sprintf(
             esc_html_x('Posted on %s', 'post date', 'awps'),
