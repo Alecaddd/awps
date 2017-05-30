31b6aaa0cdb6eb46128e2ccc05067fc4b30066bf
Alecaddd <castellani.ale@gmail.com>
2017-05-29 18:44:45 -0700

    Using browserify-shim to allow the use of global variables between modules

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ var browserSync  = require( 'browser-sync' ).create();
 var reload       = browserSync.reload;
 
 // Project related variables
+var exposeConfig = { expose: { jquery: 'jQuery' } };
 var projectURL   = 'http://wp.dev';
 
 var styleSRC     = './src/scss/style.scss';
@@ -51,69 +52,69 @@ var phpWatch     = './**/*.php';
 
 // Tasks
 gulp.task( 'browser-sync', function() {
-    browserSync.init({
-        proxy: projectURL,
-        injectChanges: true,
-        open: false
-    });
+	browserSync.init({
+		proxy: projectURL,
+		injectChanges: true,
+		open: false
+	});
 });
 
 gulp.task( 'styles', function() {
-    gulp.src( styleSRC )
-        .pipe( sourcemaps.init() )
-        .pipe( sass({
-            errLogToConsole: true,
-            outputStyle: 'compressed'
-        }) )
-        .on( 'error', console.error.bind( console ) )
-        .pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
-        .pipe( rename( { suffix: '.min' } ) )
-        .pipe( sourcemaps.write( mapURL ) )
-        .pipe( gulp.dest( styleURL ) )
-        .pipe( browserSync.stream() );
+	gulp.src( styleSRC )
+		.pipe( sourcemaps.init() )
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'compressed'
+		}) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( sourcemaps.write( mapURL ) )
+		.pipe( gulp.dest( styleURL ) )
+		.pipe( browserSync.stream() );
 });
 
 gulp.task( 'js', function() {
-    return browserify({
-        entries: [jsSRC]
-    })
-    .transform( babelify )
-    .bundle()
-    .pipe( source( 'main.min.js' ) )
-    .pipe( buffer() )
-    .pipe( gulpif( options.has( 'production' ), stripDebug() ) )
-    .pipe( sourcemaps.init({ loadMaps: true }) )
-    .pipe( uglify() )
-    .pipe( sourcemaps.write( '.' ) )
-    .pipe( gulp.dest( jsURL ) )
-    .pipe( browserSync.stream() );
+	return browserify({
+		entries: [jsSRC]
+	})
+	.transform( babelify, { presets: [ 'es2015' ] } )
+	.bundle()
+	.pipe( source( 'main.min.js' ) )
+	.pipe( buffer() )
+	.pipe( gulpif( options.has( 'production' ), stripDebug() ) )
+	.pipe( sourcemaps.init({ loadMaps: true }) )
+	.pipe( uglify() )
+	.pipe( sourcemaps.write( '.' ) )
+	.pipe( gulp.dest( jsURL ) )
+	.pipe( browserSync.stream() );
  });
 
 gulp.task( 'images', function() {
-    triggerPlumber( imgSRC, imgURL );
+	triggerPlumber( imgSRC, imgURL );
 });
 
 gulp.task( 'fonts', function() {
-    triggerPlumber( fontsSRC, fontsURL );
+	triggerPlumber( fontsSRC, fontsURL );
 });
 
 function triggerPlumber( src, url ) {
-    return gulp.src( src )
-    .pipe( plumber() )
-    .pipe( gulp.dest( url ) );
+	return gulp.src( src )
+	.pipe( plumber() )
+	.pipe( gulp.dest( url ) );
 }
 
  gulp.task( 'default', ['styles', 'js', 'images', 'fonts'], function() {
-    gulp.src( jsURL + 'main.min.js' )
-        .pipe( notify({ message: 'Assets Compiled!' }) );
+	gulp.src( jsURL + 'main.min.js' )
+		.pipe( notify({ message: 'Assets Compiled!' }) );
  });
 
  gulp.task( 'watch', ['default', 'browser-sync'], function() {
-    gulp.watch( phpWatch, reload );
-    gulp.watch( styleWatch, [ 'styles' ] );
-    gulp.watch( jsWatch, [ 'js', reload ] );
-    gulp.watch( imgWatch, [ 'images' ] );
-    gulp.watch( fontsWatch, [ 'fonts' ] );
-    gulp.src( jsURL + 'main.min.js' )
-        .pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) );
+	gulp.watch( phpWatch, reload );
+	gulp.watch( styleWatch, [ 'styles' ] );
+	gulp.watch( jsWatch, [ 'js', reload ] );
+	gulp.watch( imgWatch, [ 'images' ] );
+	gulp.watch( fontsWatch, [ 'fonts' ] );
+	gulp.src( jsURL + 'main.min.js' )
+		.pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) );
  });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
-import $ from 'jquery';
+/**
+ * Manage global libraries like jQuery or THREE from the package.json file
+ */
+var $ = require( 'jquery' );
+
+// Import libraries
 import 'slick-carousel';
+import 'izimodal';
 
 // Import custom modules
 import App from'./modules/app.js';
 import Carousel from './modules/carousel.js';
-
-// Export for others scripts to use
-window.$ = $;
 
 const app = new App();
 const carousel = new Carousel();
