924bf05a148b0b7be747d6a51ec9c1b02cb0e78e
Laurence Bahiirwa <laurence@mediauganda.com>
2017-06-10 14:32:36 +0300

    Refactoring the custom Post type
    Make CPT reusable again

--- config/custom/custom.php
+++ config/custom/custom.php
@@ -18,24 +18,29 @@ class custom
      * Create Custom Post Types
      * @return The registered post type object, or an error object
      */
-    public function custom_post_type() 
+    public function custom_post_type()
     {
+
+      $singular = 'Book';
+      $plural = 'Books';
+      $lsingular = strtolower( $singular );
+
       $labels = array(
-        'name'               => _x( 'Books', 'post type general name', 'awps' ),
-        'singular_name'      => _x( 'Book', 'post type singular name', 'awps' ),
-        'menu_name'          => _x( 'Books', 'admin menu', 'awps' ),
-        'name_admin_bar'     => _x( 'Book', 'add new on admin bar', 'awps' ),
-        'add_new'            => _x( 'Add New', 'book', 'awps' ),
-        'add_new_item'       => __( 'Add New Book', 'awps' ),
-        'new_item'           => __( 'New Book', 'awps' ),
-        'edit_item'          => __( 'Edit Book', 'awps' ),
-        'view_item'          => __( 'View Book', 'awps' ),
-        'view_items'         => __( 'View Books', 'awps' ),
-        'all_items'          => __( 'All Books', 'awps' ),
-        'search_items'       => __( 'Search Books', 'awps' ),
-        'parent_item_colon'  => __( 'Parent Books:', 'awps' ),
-        'not_found'          => __( 'No books found.', 'awps' ),
-        'not_found_in_trash' => __( 'No books found in Trash.', 'awps' )
+        'name'               => _x( $plural, 'post type general name', 'awps' ),
+        'singular_name'      => _x( $singular, 'post type singular name', 'awps' ),
+        'menu_name'          => _x( $plural, 'admin menu', 'awps' ),
+        'name_admin_bar'     => _x( $singular, 'add new on admin bar', 'awps' ),
+        'add_new'            => _x( 'Add New ' . $singular, 'awps' ),
+        'add_new_item'       => __( 'Add New ' . $singular, 'awps' ),
+        'new_item'           => __( 'New ' . $singular, 'awps' ),
+        'edit_item'          => __( 'Edit ' . $singular, 'awps' ),
+        'view_item'          => __( 'View ' . $singular, 'awps' ),
+        'view_items'         => __( 'View ' . $plural, 'awps' ),
+        'all_items'          => __( 'All ' . $plural, 'awps' ),
+        'search_items'       => __( 'Search' . $plural, 'awps' ),
+        'parent_item_colon'  => __( 'Parent ' . $plural, 'awps' ),
+        'not_found'          => __( 'No ' . $plural . 'found.', 'awps' ),
+        'not_found_in_trash' => __( 'No ' . $plural . 'found in Trash.', 'awps' )
       );
 
       $args = array(
@@ -47,7 +52,7 @@ class custom
         'show_in_menu'       => true,
         'menu_icon'          => 'dashicons-book-alt',
         'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'book' ),
+        'rewrite'            => array( 'slug' => $lsingular ),
         'capability_type'    => 'post',
         'has_archive'        => true,
         'hierarchical'       => false,
@@ -55,15 +60,15 @@ class custom
         'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
       );
 
-      register_post_type( 'book', $args );
+      register_post_type( $lsingular, $args );
     }
 
     /**
      * Flush Rewrite on CPT activation
      * @return empty
      */
-    public function rewrite_flush() 
-    {   
+    public function rewrite_flush()
+    {
         // call the CPT init function
         $this->custom_post_type();
 
