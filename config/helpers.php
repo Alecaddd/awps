28bdd5743010f1c6750d8faf84240f9fd6ac53dd
Alecaddd <castellani.ale@gmail.com>
2017-06-25 22:45:58 -0700

    Comments and description for helper function

--- config/helpers.php
+++ config/helpers.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Helpers methods
  * List all your static functions you wish to use globally on your theme
@@ -6,12 +6,18 @@
  * @package awps
  */
 
-if (!function_exists('dd')) :
-	function dd()
-	{
+if ( ! function_exists( 'dd' ) ) :
+	/**
+	 * Var_dump and die method
+	 *
+	 * @return void
+	 */
+	function dd() {
 		echo '<pre>';
-	    array_map( function( $x ) { var_dump( $x ); }, func_get_args() );
-	    echo '</pre>';
-	    die;
+		array_map( function( $x ) {
+			var_dump( $x );
+		}, func_get_args() );
+		echo '</pre>';
+		die;
 	}
 endif;
