5bb6386dc6a9bb52ba1de607bf2b5b3c3e9be450
Laurence Bahiirwa <laurence@mediauganda.com>
2018-08-03 13:14:57 +0300

    Proper echo for blog title
    use `get_bloginfo` to echo

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 							$custom_logo_id = get_theme_mod( 'custom_logo' );
 							$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 							if ( has_custom_logo() ) {
-								echo '<a href="'. esc_url( home_url( '/' ) ) .'" rel="home"><img src="'. esc_url( $logo[0] ) .'" alt=' . bloginfo( 'name' ) . '></a>';
+								echo '<a href="'. esc_url( home_url( '/' ) ) .'" rel="home"><img src="'. esc_url( $logo[0] ) .'" alt=' . get_bloginfo( 'name' ) . '></a>';
 							} else {
 								echo '<h1 class="site-title"><a href="'. esc_url( home_url( '/' ) ) .'" rel="home">' . bloginfo( 'name' ) . '</a></h1>';
 							}
