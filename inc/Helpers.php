0fbc506ada02f2bd230247d4e44c3c59ea091f02
Ortimis <jakob@drortmann.de>
2018-08-03 19:23:39 +0200

    Added Laravel Mix Versioning (#19)

--- inc/Helpers.php
+++ inc/Helpers.php
@@ -21,3 +21,60 @@ if ( ! function_exists( 'dd' ) ) :
 		die;
 	}
 endif;
+
+if (! function_exists('starts_with')) {
+    /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    function starts_with($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+if (! function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString
+     *
+     * @throws \Exception
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+		if ( !$manifestDirectory) {
+			//Setup path for standard AWPS-Folder-Structure
+			$manifestDirectory = "assets/dist/";
+		}
+        static $manifest;
+        if (! starts_with($path, '/')) {
+            $path = "/{$path}";
+		}
+        if ($manifestDirectory && ! starts_with($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+		}
+		$rootDir = dirname(__FILE__, 2);
+        if (file_exists($rootDir . '/' . $manifestDirectory.'/hot')) {
+            return "http://localhost:8080" . $path;
+        }
+        if (! $manifest) {
+			$manifestPath =  $rootDir . $manifestDirectory . 'mix-manifest.json';
+            if (! file_exists($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+			$manifest = json_decode(file_get_contents($manifestPath), true);
+		}
+		$path = $manifestDirectory . $manifest[$path];
+		return get_template_directory_uri() . $path;
+    }
+}
--- inc/Setup/Enqueue.php
+++ inc/Setup/Enqueue.php
@@ -16,6 +16,12 @@ class Enqueue
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	}
 
+	/**
+	 * Notice the mix() function in wp_enqueue_...
+	 * It provides the path to a versioned asset by Laravel Mix using querystring-based 
+	 * cache-busting (This means, the file name won't change, but the md5. Look here for 
+	 * more information: https://github.com/JeffreyWay/laravel-mix/issues/920 )
+	 */
 	public function enqueue_scripts() 
 	{
 		// Deregister the built-in version of jQuery from WordPress
@@ -24,10 +30,10 @@ class Enqueue
 		}
 
 		// CSS
-		wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/dist/css/style.css', array(), '1.0.0', 'all' );
+		wp_enqueue_style( 'main', mix('css/style.css'), array(), '1.0.0', 'all' );
 
 		// JS
-		wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/dist/js/app.js', array(), '1.0.0', true );
+		wp_enqueue_script( 'main', mix('js/app.js'), array(), '1.0.0', true );
 
 		// Activate browser-sync on development environment
 		if ( getenv( 'APP_ENV' ) === 'development' ) :
