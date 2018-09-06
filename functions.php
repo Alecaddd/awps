8693c60653f8c4d5a922c9e2529edba1633e70f7
Andriy Shabat <andriy@siteimage.com.ua>
2018-09-06 19:58:03 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  *
  * This theme uses PSR-4 and OOP logic instead of procedural coding
  * Every function, hook and action is properly divided and organized inside related folders and files
- * Use the file `config/Custom/Custom.php` to write your custom functions
+ * Use the file `inc/Custom/Custom.php` to write your custom functions
  *
  * @package awps
  */
