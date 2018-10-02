76e32fff8dec7ec654edb4b889e7bff8e0b89051
NeverDev <thomas.blanchard18@gmail.com>
2018-10-02 09:16:25 +0200

    Add theme support Woocommerce plugin

--- inc/Setup/Setup.php
+++ inc/Setup/Setup.php
@@ -30,6 +30,11 @@ class Setup
         add_theme_support( 'title-tag' );
         add_theme_support( 'post-thumbnails' );
         add_theme_support( 'customize-selective-refresh-widgets' );
+        
+        /**
+        * Add woocommerce support and woocommerce override
+        */
+        add_theme_support( 'woocommerce' );
 
         add_theme_support( 'html5', array(
             'search-form',
