3256acfa6d8cdfe74c3b86f7bf96ba3b6763d77c
Alecaddd <castellani.ale@gmail.com>
2018-11-18 15:11:23 -0800

    Properly load jquery in laravel mix, fixes #39

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,25 +9,25 @@ let mix = require( 'laravel-mix' );
 
 // BrowserSync and LiveReload on `npm run watch` command
 // Update the `proxy` and the location of your SSL Certificates if you're developing over HTTPS
-mix.browserSync({
-	proxy: 'https://wp.dev',
-	https: {
-		key: '/Users/alecaddd/.valet/Certificates/wp.dev.key',
-		cert: '/Users/alecaddd/.valet/Certificates/wp.dev.crt'
-	},
-	files: [
-		'**/*.php',
-		'assets/dist/css/**/*.css',
-		'assets/dist/js/**/*.js'
-	],
-	injectChanges: true,
-	open: false
-});
+// mix.browserSync({
+// 	proxy: 'https://your-local-domain',
+// 	https: {
+// 		key: '/your/certificates/location/your-local-domain.key',
+// 		cert: '/your/certificates/location/your-local-domain.crt'
+// 	},
+// 	files: [
+// 		'**/*.php',
+// 		'assets/dist/css/**/*.css',
+// 		'assets/dist/js/**/*.js'
+// 	],
+// 	injectChanges: true,
+// 	open: false
+// });
 
 // Autloading jQuery to make it accessible to all the packages, because, you know, reasons
 // You can comment this line if you don't need jQuery
 mix.autoload({
-	'jquery': ['jQuery', '$']
+	jquery: ['$', 'window.jQuery', 'jQuery'],
 });
 
 mix.setPublicPath( './assets/dist' );
