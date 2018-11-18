87a2ea3ede12b35fa7908be5ac12bd66fd8b2d02
Alecaddd <castellani.ale@gmail.com>
2018-11-18 15:19:14 -0800

    eslint fix for latest-post block

--- assets/src/scripts/blocks/latest-post.js
+++ assets/src/scripts/blocks/latest-post.js
@@ -5,24 +5,26 @@ registerBlockType( 'gutenberg-test/latest-post', {
 	title: 'Latest Post',
 	icon: 'megaphone',
 	category: 'widgets',
-	
+
 	edit: withSelect( ( select ) => {
 		return {
-		    posts: select( 'core' ).getEntityRecords( 'postType', 'post' )
-        	};
-	} )( ( { posts, className } ) => {
-		if ( posts && posts.length === 0 ) {
-		    return "No posts";
-		}
+			posts: select( 'core' ).getEntityRecords( 'postType', 'post' )
+		};
+	})( ({ posts, className }) => {
 		var post = posts[ 0 ];
 
+		if ( posts && 0 === posts.length ) {
+			return 'No posts';
+		}
+
 		return <a className={ className } href={ post.link }>
-			    { post.title.rendered }
-			</a>;
+			{ post.title.rendered }
+		</a>;
 	}),
 
-    	save() {
+	save() {
+
 		// Rendering in PHP
 		return null;
-    	}
+	}
 });
